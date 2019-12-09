8d82ee792f004c11771e6742fc59554cd6c7b6bd
Connor Davis <connormdavis99@users.campus>
2019-12-09 16:18:08 -0600

    classwork

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,56 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+
+            Vector3 one = new Vector3(3, 4, 5);
+            Vector3 two = new Vector3(4, 8, 0);
+
+            Console.WriteLine("\nAddition");
+            Console.WriteLine($"Static method Add: {Vector3.Add(one, two)}");
+            Console.WriteLine($"Object method Add: {one.Add(two)}");
+            Console.WriteLine($"Operator: {one + two}");
+
+            Console.WriteLine("\nSubtraction");
+            Console.WriteLine($"Operator: {one - two}");
+
+            Console.WriteLine("\nScaler");
+            Console.WriteLine($"Operator: {2 * one}");
+            Console.WriteLine($"Operator: {two * 2}");
+
+            Console.WriteLine("\nComparison");
+            Console.WriteLine($"Operator -: {one == two}");
+            Console.WriteLine($"Operator +: {one == new Vector3(3, 4, 5)}");
+            Console.WriteLine($"Operator +: {one != two}");
+            Console.WriteLine($"Operator -: {one != new Vector3(3, 4, 5)}");
+
+            Console.WriteLine("\nIncerment");
+            Console.WriteLine($"Pre: {++one}");
+            Console.WriteLine($"Post: {one++}");
+
+            Console.WriteLine("\nMagnigtude");
+            Console.WriteLine($"Operator: {~one}");
+
+            Console.Write($"{one} is ");
+            if (one)
+            {
+                Console.Write("truthy");
+            }
+            else
+            {
+                Console.Write("falsy");
+            }
+
+            Console.WriteLine();
+
+            Console.Write($"{Vector3.Zero} is ");
+            if (Vector3.Zero)
+            {
+                Console.Write("truthy");
+            }
+            else
+            {
+                Console.Write("falsy");
+            }
         }
     }
 }
--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -19,10 +19,84 @@ namespace VectorMath
 
         // Vector Addition
 
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public Vector3 Add(Vector3 other)
+        {
+            return new Vector3(other.X + this.X, other.Y + this.Y, other.Z + this.Z);
+        }
+
+        public void AddInPlace(Vector3 other)
+        {
+            this.X += other.X;
+            this.Y += other.Y;
+            this.Z += other.Z;
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
         // Vector Subtraction 
+
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
 
         // Vector Scaling 
 
+        public static Vector3 operator *(double s, Vector3 v)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
+
+        public static Vector3 operator *(Vector3 v, double s)
+        {
+            return new Vector3(s * v.X, s * v.Y, s * v.Z);
+        }
+
         // Vector Comparison
+
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return (a.X == b.X && a.Y == b.Y && a.Z == b.Z);
+        }
+
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return !(a == b);
+        }
+
+        public static Vector3 operator ++(Vector3 v)
+        {
+            return new Vector3(++v.X, ++v.Y, ++v.Z);
+        }
+
+        public static double operator ~(Vector3 v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
+        }
+
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
+        public static bool operator true(Vector3 v)
+        {
+            return Zero != v;
+        }
+
+        public static bool operator false(Vector3 v)
+        {
+            return Zero == v;
+        }
+
+        public override string ToString()
+        {
+            return $"<{this.X}, {this.Y}, {this.Z}>";
+        }
     }
 }
